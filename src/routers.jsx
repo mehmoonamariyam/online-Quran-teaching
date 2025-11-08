359cb85b554f385ed2c604f391525138c5c5507e
mehmoonamaariyam <mehmoonamaryam086@gmail.com>
2025-11-08 16:28:30 +0500

    update

--- src/routers.jsx
+++ src/routers.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/homepage'
 
-const RountersCall = () => {
+const RoutersCall = () => {
   return (
     <Routes><Route path='' element={<HomePage/>}/></Routes>
   )
 }
 
-export default RountersCall
+export default RoutersCall
