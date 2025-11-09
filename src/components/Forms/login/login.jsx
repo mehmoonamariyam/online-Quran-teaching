592f586afae90d8c32df2fef6db3f78b60c183ac
mehmoonamaariyam <mehmoonamaryam086@gmail.com>
2025-11-09 17:30:08 +0500

    add

--- src/components/Forms/login/login.jsx
+++ src/components/Forms/login/login.jsx
@@ -27,12 +27,12 @@ const LoginForm = () => {
 
         {/* intro portion */}
 
-        <div> <img src="/images/loginb.png" alt="Logintop"
+        <div> <img src="/images/bismillah.png" alt="Logintop"
           className="w-[400px] h-[90px] items-center pl-6.5" /></div>
 
           <div> <img src="/images/loginq.png" alt="Logintop"
           className="w-[350px] h-[150px] items-center pl-8.5" />
-          <h1 className="text-pink-950 text-3xl font-bold pl-10.5 py-0 pt-0">NABAH-AL-JANAH</h1></div>
+          <h1 className="text-pink-950 text-3xl font-bold pl-10.5 py-0 pt-0">NABA-AL-JANNAH</h1></div>
 
 
         <h2 className="text-2xl font-bold text-center mb-6 text-gray-700"></h2>
