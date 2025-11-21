0bc2bb4c212d3cc394fa6106725e69afd65f473e
fatima-nigum <nigumfatimaa@gmail.com>
2025-11-21 19:50:56 +0500

    Save local changes before pulling

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -3,13 +3,13 @@ import courseReducer from "./slice/courseSlice";
 import LoginSlice from "./slice/FormSlices/login"; 
 import SignupSlice from "./slice/FormSlices/signup";
 import enrollSlice from "./slice/FormSlices/enroll";
-import courseSlice from "./slice/courseSlice";
+
 import tutorsSlice from "./slice/TutorSlice";
 
 
 export const store = configureStore({
   reducer: {
-    courses: courseReducer,
+    
     login: LoginSlice.reducer,
     signup: SignupSlice.reducer,
     enroll: enrollSlice.reducer,
